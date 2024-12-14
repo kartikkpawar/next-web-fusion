fcdbcefb9537f7723d6255a8c8c1fb122d3a824c
kartikkpawar <knpwar789@gmail.com>
2024-12-14 12:20:31 +0530

    feat: layer source code

--- app/editor/_components/ItemSource.tsx
+++ app/editor/_components/ItemSource.tsx
@@ -12,23 +12,32 @@ import {
 import { Button } from "@/components/ui/button";
 
 import { generateComponentSourceCode } from "@/lib/codeGenration";
-import { Code } from "lucide-react";
+import { Check, Code, Copy, XIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { toast } from "sonner";
 
 function ItemSourcedialog() {
   const { currentActiveElement } = useElements();
   const [codeString, setcodeString] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (currentActiveElement) {
-      const code = generateComponentSourceCode([currentActiveElement!]);
+      const code = generateComponentSourceCode([currentActiveElement]);
       setcodeString(code);
     }
   }, [currentActiveElement]);
   return (
-    <AlertDialog>
+    <AlertDialog
+      onOpenChange={(open) => {
+        setCopied(false);
+        setOpen(open);
+      }}
+      open={open}
+    >
       <AlertDialogTrigger disabled={!currentActiveElement}>
         <Button variant={"outline"} disabled={!currentActiveElement}>
           <Code />
@@ -38,24 +47,55 @@ function ItemSourcedialog() {
       <AlertDialogContent className="max-w-screen-xl w-full">
         <AlertDialogHeader>
           <AlertDialogTitle>
-            <AlertDialogCancel className="self-end">Cancel</AlertDialogCancel>
+            <div className="flex w-full justify-between items-center">
+              Code Snippet
+              <AlertDialogCancel>
+                <XIcon />
+              </AlertDialogCancel>
+            </div>
           </AlertDialogTitle>
           <AlertDialogDescription className="w-full max-w-screen-xl">
-            <SyntaxHighlighter
-              language="jsx"
-              style={atomDark}
-              showLineNumbers
-              wrapLongLines={true}
-              wrapLines={true}
-              codeTagProps={{
-                style: {
-                  boxSizing: "border-box",
-                  textWrap: "wrap",
-                },
-              }}
-            >
-              {codeString}
-            </SyntaxHighlighter>
+            <div className="w-full relative">
+              <div>
+                <div className="flex items-center gap-1 absolute top-2 right-2">
+                  {copied ? (
+                    <>
+                      <Check size={18} />
+                      Copied!
+                    </>
+                  ) : (
+                    <Copy
+                      size={20}
+                      className="cursor-pointer"
+                      onClick={() => {
+                        navigator.clipboard
+                          .writeText(codeString)
+                          .then(() => {
+                            setCopied(true);
+                          })
+                          .catch(() => {
+                            toast.error("Unable to copy ", { id: "code-copy" });
+                          });
+                      }}
+                    />
+                  )}
+                </div>
+              </div>
+              <SyntaxHighlighter
+                language="jsx"
+                style={atomDark}
+                wrapLongLines={true}
+                wrapLines={true}
+                codeTagProps={{
+                  style: {
+                    boxSizing: "border-box",
+                    textWrap: "wrap",
+                  },
+                }}
+              >
+                {codeString}
+              </SyntaxHighlighter>
+            </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
       </AlertDialogContent>
